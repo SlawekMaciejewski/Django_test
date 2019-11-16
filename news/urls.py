4f6bbccdcf8a89118deaeb6310b4a8f2387cf00d
SlawekMaciejewski <slawek74.m@interia.pl>
2019-11-16 11:25:19 +0100

    Add ListView, TemplateView

--- news/urls.py
+++ news/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path('list/', views.post_list, name='post_list'),
+    # path('', views.home_page, name='home_page'),
+    path('', views.HomePage.as_view(), name='home_page'),
+    #path('list/', views.post_list, name='post_list'),
+    path('list/', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.post_detail, name='post_detail'),
     path('author/', views.author, name='author'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import HttpResponse, render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, TemplateView
 from .models import Post
 
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 4
+    template_name = 'news/post/list.html'
+
+class HomePage(TemplateView):
+    template_name = 'home.html'
+
+
 def home_page(request):
-    return render(request, './home.html')
+    return render(request, 'home.html')
 
 
 def post_list(request):
