10477ce095af9bc71eaebbbc60c7a2b030ce3d2a
SlawekMaciejewski <slawek74.m@interia.pl>
2019-11-17 10:17:37 +0100

    Add account/urls.py, templates/account/login.html and fill it

--- TreningDjango/urls.py
+++ TreningDjango/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
     path('', include('news.urls')),
     re_path(r'^news/', include('news.urls')),
 ]
--- account/views.py
+++ account/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import LogInForm
 
-# Create your views here.
 
+# Create your views here.
 
 
 def log_in(request):
     if request.method == 'POST':
         form = LogInForm(request.POST)
         if form.is_valid():
-            data = form.changed_data
+            data = form.cleaned_data
             user = authenticate(username=data['username'],
                                 password=data['password'])
             if user is not None:
