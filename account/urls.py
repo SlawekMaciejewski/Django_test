c39e50ed0e4dba7017c8e8df9cc82bcceb08a9be
SlawekMaciejewski <slawek74.m@interia.pl>
2019-11-17 16:01:22 +0100

    Add decorator login_required

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required # dzięki temu przenosi nas do logowania jak nie jesteśmy zalogowani
 from . import views
 
 urlpatterns = [
     path('login/', views.log_in, name='login'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('my_account/', views.UserView.as_view(), name='my_account'),
+    # path('my_account/', views.UserView.as_view(), name='my_account'),
+    path('my_account/', login_required(views.UserView.as_view()), name='my_account'),
+
 ]
