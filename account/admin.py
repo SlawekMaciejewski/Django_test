e2c34579f04e24ad02c8b1c83558796a9831efaa
SlawekMaciejewski <slawek74.m@interia.pl>
2019-11-24 09:55:42 +0100

    Add user profil

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfil
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth']
+
+admin.site.register(UserProfil)
--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,15 @@
 from django import forms
+from .models import UserProfil
 
 
 class LogInForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserProfilForm(forms.ModelForm):
+    class Meta:
+        user_profil = UserProfil
+        fields = ('date_of_birth',)
+
 
 
